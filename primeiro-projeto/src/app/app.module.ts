9b0c39dac85ca6fbf61da381236b317d099a854b
Alfredo Stanquini Neto <stanquini@gmail.com>
2018-01-10 20:07:26 -0200

    trabalhando com modulos

--- primeiro-projeto/src/app/app.module.ts
+++ primeiro-projeto/src/app/app.module.ts
@@ -1,23 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
-
 import { AppComponent } from './app.component';
 import {MeuPrimeiroComponent} from './meu-primeiro/meu-primeiro.component';
 import { MeuPrimeiroAngularcliComponent } from './meu-primeiro-angularcli/meu-primeiro-angularcli.component';
-
+import { CursosModule } from './cursos/cursos.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MeuPrimeiroComponent,
-    MeuPrimeiroAngularcliComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        MeuPrimeiroComponent,
+        MeuPrimeiroAngularcliComponent
+    ],
+    imports: [
+        BrowserModule,
+        CursosModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
+
 export class AppModule { }
