14943b28ec4832325f31af41e36a524581158365
Alfredo Stanquini Neto <stanquini@gmail.com>
2018-01-10 19:37:06 -0200

    hello-world com TypeScript

---
+++ hello-typescript/main.js
@@ -0,0 +1,16 @@
+var minhaVar = 'minha variavel';
+var num = 2;
+var PI = 3.14;
+var numeros = [1, 2, 3];
+numeros.map(function (valor) { return valor * 2; });
+var Matematica = /** @class */ (function () {
+    function Matematica() {
+    }
+    Matematica.prototype.soma = function (x, y) {
+        return x + y;
+    };
+    return Matematica;
+}());
+var n1 = 'Ola';
+//n1 = 5;
+n1 = 'Ola amigo';
---
+++ hello-typescript/main.ts
@@ -0,0 +1,18 @@
+var minhaVar = 'minha variavel';
+
+let num = 2;
+const PI = 3.14;
+
+let numeros = [1,2,3];
+numeros.map(valor=> valor*2);
+
+class Matematica {
+
+    soma(x,y) {
+        return x +y
+    }
+}
+
+let n1:string = 'Ola';
+//n1 = 5;
+n1 = 'Ola amigo';
