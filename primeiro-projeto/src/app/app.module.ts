3561d5d5eec713ff3308cc4c8f8e1c45ba883835
Alfredo Stanquini Neto <stanquini@gmail.com>
2018-01-10 18:50:16 -0200

    adicionando e confugurando o primiro componente

--- primeiro-projeto/src/app/app.module.ts
+++ primeiro-projeto/src/app/app.module.ts
@@ -2,12 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
+
 import { AppComponent } from './app.component';
+import {MeuPrimeiroComponent} from './meu-primeiro/meu-primeiro.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeuPrimeiroComponent
   ],
   imports: [
     BrowserModule
---
+++ primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,10 @@
+'use strict';
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'meu-primeiro-component',
+    template: `
+    <p>Meu Primeiro component com Angular 2</p>
+    `
+})
+export class MeuPrimeiroComponent { }
