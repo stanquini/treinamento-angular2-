0d0c40ba0c57fa4c9f3abeb59313ed96701d330f
Alfredo Stanquini Neto <stanquini@gmail.com>
2018-01-11 15:51:13 -0200

    templates de cursos

--- primeiro-projeto/src/app/cursos/cursos.component.ts
+++ primeiro-projeto/src/app/cursos/cursos.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-cursos',
-  templateUrl: './cursos.component.html',
-  styleUrls: ['./cursos.component.css']
+    selector: 'app-cursos',
+    templateUrl: './cursos.component.html',
+     styleUrls: ['./cursos.component.css']
 })
 export class CursosComponent implements OnInit {
 
-  constructor() { }
+    nomePortal: string;
 
-  ngOnInit() {
-  }
+    cursos: string[] = ['Java', 'Ext JS', 'Angular']; 
+
+    constructor() { 
+        
+        this.nomePortal = 'http://loiane.training';
+    } 
+
+    ngOnInit() {
+    }
 
 }
--- primeiro-projeto/src/app/cursos/cursos.module.ts
+++ primeiro-projeto/src/app/cursos/cursos.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { CursosComponent } from './cursos.component';
 import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 
@@ -7,7 +8,10 @@ import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
   imports: [
     CommonModule
   ],
-  declarations: [CursosComponent, CursoDetalheComponent],
+  declarations: [
+    CursosComponent, 
+    CursoDetalheComponent
+  ],
   exports: [
     CursosComponent
   ]
