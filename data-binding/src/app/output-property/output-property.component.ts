d2eeaf2eabcd5c181c57d94124c7cb979a356445
Alfredo Stanquini Neto <stanquini@gmail.com>
2018-01-15 21:24:39 -0200

    acesso ao DOM e ao template ViewChild

--- data-binding/src/app/output-property/output-property.component.ts
+++ data-binding/src/app/output-property/output-property.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
+
 
 @Component({
   selector: 'contador',
@@ -13,15 +14,20 @@ export class OutputPropertyComponent implements OnInit {
 
   @Output() mudouValor = new EventEmitter();
 
+  @ViewChild('campoInput') campoValorInput: ElementRef;
+
   decrementa() {
 
     this.valor--;
+    //this.campoValorInput.nativeElement.value--;
     this.mudouValor.emit({novoValor: this.valor});
   }
 
   incrementa() {
     
+
     this.valor++;
+    //this.campoValorInput.nativeElement.value++;
     this.mudouValor.emit({novoValor: this.valor});
   }
 
