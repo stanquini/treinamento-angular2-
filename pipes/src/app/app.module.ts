a0f445c356744a036ac0d3bb2f77153ff1aeb8cc
Alfredo Stanquini Neto <stanquini@gmail.com>
2018-01-26 18:59:38 -0200

    add internaciolizacao

--- pipes/src/app/app.module.ts
+++ pipes/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, LOCALE_ID } from '@angular/core';
+import ptBr from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { ExemplosPipesComponent } from './exemplos-pipes/exemplos-pipes.component';
 import { CamelCasePipe } from './camel-case.pipe';
+import { SettingsService } from './settings.service';
 
-
+registerLocaleData(ptBr)
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +18,19 @@ import { CamelCasePipe } from './camel-case.pipe';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    /*{
+      provide: LOCALE_ID,
+      useValue: 'pt-BR'
+    }*/
+    SettingsService,
+    {
+      provide: LOCALE_ID,
+      deps: [SettingsService],
+      useFactory: (SettingsService) => SettingsService.getLocale()
+    }
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
