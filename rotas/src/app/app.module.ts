9c95f490c0e48bcce383fd21d9a82561ba1464b1
Alfredo Stanquini Neto <stanquini@gmail.com>
2018-01-29 18:21:54 -0200

    add rotas simples

--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -3,16 +3,24 @@ import { NgModule } from '@angular/core';
 import { MaterializeModule } from 'angular2-materialize';
 
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { routing } from './app.routing';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    CursosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
---
+++ rotas/src/app/app.routing.ts
@@ -0,0 +1,14 @@
+import { Routes, RouterModule } from '@angular/router';
+import { ModuleWithProviders } from '@angular/core';
+
+import { HomeComponent } from './home/home.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { LoginComponent } from './login/login.component';
+
+const APP_ROUTES: Routes = [
+    { path: 'cursos', component: CursosComponent},
+    { path: 'login', component: LoginComponent},
+    { path: '', component: HomeComponent}
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(APP_ROUTES);
