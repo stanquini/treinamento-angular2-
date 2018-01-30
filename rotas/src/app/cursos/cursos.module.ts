35c6e625324f580cfdad640643559d92fee83617
Alfredo Stanquini Neto <stanquini@gmail.com>
2018-01-30 16:35:39 -0200

    criacao de modulo de rotas e funcionalidade

--- rotas/src/app/cursos/cursos.module.ts
+++ rotas/src/app/cursos/cursos.module.ts
@@ -1,16 +1,16 @@
-import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
-import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 import { CursosComponent } from './cursos.component';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
 import { CursosService } from './cursos.service';
+import { CursosRoutingModule } from './cursos.routing.module';
 
 @NgModule({
     imports: [
         CommonModule,
-        RouterModule
+        CursosRoutingModule
     ],
     exports: [],
     declarations: [
