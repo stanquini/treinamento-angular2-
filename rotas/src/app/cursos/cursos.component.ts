1f90d871584432847d04a51d080b31a98eba5088
Alfredo Stanquini Neto <stanquini@gmail.com>
2018-01-30 15:16:08 -0200

    modulo de rotas

--- rotas/src/app/cursos/cursos.component.ts
+++ rotas/src/app/cursos/cursos.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs/Rx';
 
 import { CursosService } from './cursos.service';
+
 
 
 @Component({
@@ -11,12 +14,33 @@ import { CursosService } from './cursos.service';
 export class CursosComponent implements OnInit {
 
   cursos: any[];
+  pagina: number;
+  inscricao: Subscription;
 
-  constructor(private cursosService: CursosService) { }
+  constructor(
+    private cursosService: CursosService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit() {
 
     this.cursos = this.cursosService.getCursos();
+    this.inscricao = this.route.queryParams.subscribe(
+      (queryParams: any) => {
+        this.pagina = queryParams['pagina'];
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    
+    this.inscricao.unsubscribe();
+  }
+
+  proximaPagina() {
+    //this.pagina++;
+    this.router.navigate(['cursos'], {queryParams: { pagina: ++this.pagina}});
   }
 
 }
