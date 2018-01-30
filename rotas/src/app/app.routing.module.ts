958624fffc077b3c5faa8bf8fff72b4569f880f8
Alfredo Stanquini Neto <stanquini@gmail.com>
2018-01-30 15:53:02 -0200

    add modulo de funcionalidade

--- rotas/src/app/app.routing.module.ts
+++ rotas/src/app/app.routing.module.ts
@@ -5,8 +5,8 @@ import { ModuleWithProviders } from '@angular/core';
 import { HomeComponent } from './home/home.component';
 import { CursosComponent } from './cursos/cursos.component';
 import { LoginComponent } from './login/login.component';
-import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
-import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
+import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
+import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
 
 const appRoutes: Routes = [
     { path: 'cursos', component: CursosComponent},
@@ -20,4 +20,5 @@ const appRoutes: Routes = [
     imports: [RouterModule.forRoot(appRoutes)],
     exports: [RouterModule]
 })
+
 export class AppRoutingModule {}
--- rotas/src/app/cursos/curso-detalhe/curso-detalhe.component.ts
+++ rotas/src/app/cursos/curso-detalhe/curso-detalhe.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs/Subscription';
 
-import { CursosService } from './../cursos/cursos.service';
+import { CursosService } from '../cursos.service';
 
 
 @Component({
