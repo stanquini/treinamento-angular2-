36302574b7e809678411e8841d2aaba622508755
Alfredo Stanquini Neto <stanquini@gmail.com>
2018-01-31 20:07:37 -0200

    tela de login

--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { CursosComponent } from './cursos/cursos.component';
 import { AppRoutingModule } from './app.routing.module';
+import { AuthService } from './login/auth.service';
 //import { CursosModule } from './cursos/cursos.module';
 //import { AlunosModule } from './alunos/alunos.module';
 
@@ -23,6 +24,9 @@ import { AppRoutingModule } from './app.routing.module';
     //CursosModule,
   //  AlunosModule,
     AppRoutingModule
+  ],
+  providers: [
+    AuthService
   ],
   bootstrap: [AppComponent]
 })
